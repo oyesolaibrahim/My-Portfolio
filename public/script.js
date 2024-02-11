e7e8a5aa4a7b5e264bca70a40289525466ccc228
Ibrahim <oyesolaibrahim14@gmail.com>
2024-02-11 23:37:13 +0100

    Uncompleted portfolio

---
+++ public/script.js
@@ -0,0 +1,53 @@
+
+   const form = document.querySelector("#form")
+   const scriptURL = 'https://script.google.com/macros/s/AKfycbweO9bIkKpG_4we13g1uFOPrdcJgrriZJwdnB6CwW0pYlM7Atm73oKrrrXJHM0dxXRi3w/exec'
+   const submitButton = document.querySelector("#submit")
+   const Message = document.getElementById("msgDelivered")
+
+    form.addEventListener('submit', e => {
+        submitButton.disabled = true
+        e.preventDefault()
+    
+        const responseBody = new FormData(form)
+        const TrendBody = responseBody.getAll("TRENDS").join(" ")
+        responseBody.set('TRENDS', TrendBody)
+        fetch(scriptURL, { method: 'POST', body: responseBody })
+        .then(response => {
+        Message.innerHTML = "Message sent successfully!!!"
+        setTimeout(function() {
+            Message.innerHTML = ""
+        },5000)
+        form.reset()      
+    })
+       .catch(error => {
+        alert('Error!', error.message)
+        submitButton.disabled = false
+}
+       )
+    }) 
+
+           
+   
+function themeOpen() {
+	const theme = document.querySelector("#theme-img");
+	const close = document.querySelector(".fa-times");
+	const themeDiv = document.querySelector(".theme-div");
+    themeDiv.style.right = "10px";
+    themeDiv.style.width = "15%";
+    themeDiv.style.padding = "10px";
+    themeDiv.style.height = "10vh";
+    theme.style.display = "none";
+    close.style.display = "inline-block";
+}
+
+function themeClose() {
+	const theme = document.querySelector("#theme-img");
+	const close = document.querySelector(".fa-times");
+	const themeDiv = document.querySelector(".theme-div");
+    themeDiv.style.right = "20px";
+    themeDiv.style.width = "0%";
+    themeDiv.style.padding = "0px";
+    themeDiv.style.height = "0vh";
+    theme.style.display = "inline-block";
+    close.style.display = "none";
+}
